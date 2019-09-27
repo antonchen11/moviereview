dbb1c87fddb968f6d5b9583780e459b3100b8492
Anton Chen <achen@pixability.com>
2019-09-27 13:00:13 -0700

    my first step

--- movies/views.py
+++ movies/views.py
@@ -3,13 +3,22 @@ from django.contrib import messages
 from airtable import Airtable #google spreadsheet on steroids
 import os # using for environment vars
 
+#Connect to airtable database
+AT = Airtable(os.environ.get('AIRTABLE_MOVIESTABLE_BASE_ID'),
+                'Movies',
+                api_key=os.environ.get('AIRTABLE_API_KEY'))
+
 # Create your views here.
 #homepage is a funciton that takes in the request as an argument.
 #render that request and show/run the following html file.
 def home_page(request):
-    return render(request, 'movies/movies_stuff.html')
+    # print(str(request.GET.get('query', '')))
+    user_query = str(request.GET.get('query', '')) #Get method and pull from html text box of query
+    search_result = AT.get_all(formula="FIND('" + user_query.lower() + "', LOWER({Name}))")
+    stuff_for_frontend = {'search_result': search_result} #context dictionary of K:V
+    return render(request, 'movies/movies_stuff.html', stuff_for_frontend)
     
-# Connect the above path under urls.py
+    # Connect the above path under urls.py
     
     
 
