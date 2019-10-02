ae9a23b48ce43e57d89c7f875f28a19936bfd788
Anton Chen <achen@pixability.com>
2019-10-02 14:57:45 -0700

    created movie details edit form

--- movies/views.py
+++ movies/views.py
@@ -31,3 +31,10 @@ def create(request):
         }
         AT.insert(data)
     return redirect('/')
+
+# def edit(request):
+#     if request.method == 'POST':
+#         data = {
+# 
+#         }
+#         AT.edit(data)
